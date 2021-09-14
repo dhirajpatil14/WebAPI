c953fe6008591c625b3e90686ef09c8e524cc690
Tushar Patil <tusharpatil3790@gmail.com>
2021-09-14 22:48:31 +0530

    Add project files.

---
+++ WebAPI/Controllers/DepartmentController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public DepartmentController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult DeptGet()
+        {
+            try
+            {
+                string query = @"
+                select DepartmentId, DepartmentName from department
+                ";
+                DataTable dt = new DataTable();
+                string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+                SqlDataReader Reader;
+                using (SqlConnection con = new SqlConnection(sqlDataSource))
+                {
+                    con.Open();
+                    using (SqlCommand cmd = new SqlCommand(query, con))
+                    {
+                        Reader = cmd.ExecuteReader();
+                        dt.Load(Reader);
+
+                        Reader.Close();
+                        con.Close();
+                    }
+                }
+
+                return new JsonResult(dt);
+            }
+            catch (Exception ex)
+            {
+                return new JsonResult(ex.Message);
+            }
+        }
+    }
+}
