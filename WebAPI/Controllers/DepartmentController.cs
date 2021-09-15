0a7d1f774799687b990257c965ff10f0d4efb459
Tushar Patil <tusharpatil3790@gmail.com>
2021-09-15 23:51:29 +0530

    Added Post, delete, put method

--- WebAPI/Controllers/DepartmentController.cs
+++ WebAPI/Controllers/DepartmentController.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using System.Data.SqlClient;
 using System.Data;
+using WebAPI.Models;
 
 namespace WebAPI.Controllers
 {
@@ -46,6 +47,107 @@ namespace WebAPI.Controllers
                 }
 
                 return new JsonResult(dt);
+            }
+            catch (Exception ex)
+            {
+                return new JsonResult(ex.Message);
+            }
+        }
+
+
+        [HttpPost]
+        public JsonResult DeptPost(Department Dept)
+        {
+            try
+            {
+                string query = @"
+                Insert into dbo.Department values(
+                '" + Dept.DepartmentName + @"'    
+                )
+                ";
+                DataTable dt = new DataTable();
+                string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+                SqlDataReader Reader;
+                using (SqlConnection con = new SqlConnection(sqlDataSource))
+                {
+                    con.Open();
+                    using (SqlCommand cmd = new SqlCommand(query, con))
+                    {
+                        Reader = cmd.ExecuteReader();
+                        dt.Load(Reader);
+
+                        Reader.Close();
+                        con.Close();
+                    }
+                }
+                return new JsonResult("Added Successfully");
+            }
+            catch (Exception ex)
+            { 
+             return new JsonResult(ex.Message); 
+            }
+        }
+
+
+        [HttpPut]
+        public JsonResult DeptPut(Department Dept)
+        {
+            try
+            {
+                string query = @"
+                    Update dbo.Department 
+                    set DepartmentName = '" + Dept.DepartmentName+ @"'
+                    where DepartmentId = '" + Dept.DepartmentId + @"'
+                ";
+                DataTable dt = new DataTable();
+                string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+                SqlDataReader Reader;
+                using (SqlConnection con = new SqlConnection(sqlDataSource))
+                {
+                    con.Open();
+                    using (SqlCommand cmd = new SqlCommand(query, con))
+                    {
+                        Reader = cmd.ExecuteReader();
+                        dt.Load(Reader);
+
+                        Reader.Close();
+                        con.Close();
+                    }
+                }
+                return new JsonResult("Updated Successfully");
+            }
+            catch (Exception ex)
+            {
+                return new JsonResult(ex.Message);
+            }
+        }
+
+
+        [HttpDelete("{DepartmentId}")]
+        public JsonResult DeptDelete(int DepartmentId)
+        {
+            try
+            {
+                string query = @"
+                    Delete from dbo.Department 
+                    where DepartmentId = '" + DepartmentId + @"'
+                ";
+                DataTable dt = new DataTable();
+                string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+                SqlDataReader Reader;
+                using (SqlConnection con = new SqlConnection(sqlDataSource))
+                {
+                    con.Open();
+                    using (SqlCommand cmd = new SqlCommand(query, con))
+                    {
+                        Reader = cmd.ExecuteReader();
+                        dt.Load(Reader);
+
+                        Reader.Close();
+                        con.Close();
+                    }
+                }
+                return new JsonResult("Deleted Successfully");
             }
             catch (Exception ex)
             {
